9390b96ff62a58859ed8186e587300f9a31b1ac4
Isaac Overstreet <isaacoverstreet3@gmail.com>
2022-03-03 00:09:56 -0800

    did the homework

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -6,9 +6,9 @@ namespace ToDoList.Models
   {
     public string Description { get; set; }
     public int Id { get; }
-    private static List<Item> _instances = new List<Item> {};
+    private static List<Item> _instances = new List<Item> { };
 
-    public Item (string description)
+    public Item(string description)
     {
       Description = description;
       _instances.Add(this);
@@ -24,11 +24,9 @@ namespace ToDoList.Models
     {
       _instances.Clear();
     }
-
     public static Item Find(int searchId)
     {
-      return _instances[searchId - 1];
+      return _instances[searchId-1];
     }
-
   }
 }
